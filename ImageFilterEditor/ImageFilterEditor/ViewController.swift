3fa3c71281ede50fa23dd3856ad75f62979df289
Norlan Tibanear <ntibanear@gmail.com>
2020-10-28 23:00:28 -0400

    starting

---
+++ ImageFilterEditor/ImageFilterEditor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFilterEditor
+//
+//  Created by Norlan Tibanear on 10/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
